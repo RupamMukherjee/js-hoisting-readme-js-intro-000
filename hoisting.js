9f4455ed42fb1f42d1818563a0d08fed7fa88ada
Rupam Mukherjee <rupam15497@gmail.com>
2019-01-19 20:11:07 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,24 +8,24 @@ function callMe() {
     return lyric
   }
   console.log(callMe())
-  
+
   function crazy() {
     // fix the code in here:
-    
+
     var thisIsCrazy = function (){
       console.log("hey!!!")
-  
+
     }
     thisIsCrazy()
   }
   crazy()
-  
+
   function sayMyName() {
     // fix the code in here:
     var name = "Cricky";
-  
+
     sayMy();
-  
+
     function sayMy() {
       name = "Kristin";
       console.log(name);
